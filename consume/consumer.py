2070a5c947514dfcc70436eeae3d8b6a6ed01caf
Race Peterson <racepeterson@MF00005.local>
2022-11-06 11:58:46 -0700

    producer dc fin

--- consume/consumer.py
+++ consume/consumer.py
@@ -1,6 +1,6 @@
 from confluent_kafka import Consumer
 ################
-c=Consumer({'bootstrap.servers':'localhost:9092','group.id':'python-consumer','auto.offset.reset':'earliest'})
+c = Consumer({'bootstrap.servers':'localhost:9092','group.id':'python-consumer','auto.offset.reset':'earliest'})
 print('Kafka Consumer has been initiated...')
 print('Available topics to consume: ', c.list_topics().topics)
 c.subscribe(['user-tracker'])
